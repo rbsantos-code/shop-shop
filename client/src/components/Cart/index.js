93dfcad88bd5782d9d9774329d4af1c3032d1cbe
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-06-25 14:35:36 -0700

    fixing checkout

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,14 +1,27 @@
-import React, { useEffect } from 'react';
-import CartItem from '../CartItem';
-import Auth from '../../utils/auth';
-import './style.css';
+import React, { useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { useLazyQuery } from '@apollo/client';
+import { QUERY_CHECKOUT } from "../../utils/queries"
+import { idbPromise } from "../../utils/helpers"
+import CartItem from "../CartItem";
+import Auth from "../../utils/auth";
+import { useStoreContext } from "../../utils/GlobalState";
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
+import "./style.css";
 
-import { useStoreContext } from '../../utils/GlobalState';
-import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
-import { idbPromise } from '../../utils/helpers';
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session })
+      })
+    }
+  }, [data]);
 
   useEffect(() => {
     async function getCart() {
@@ -20,11 +33,32 @@ const Cart = () => {
       getCart();
     }
   }, [state.cart.length, dispatch]);
-  
+
 
   function toggleCart() {
     dispatch({ type: TOGGLE_CART });
+  }
 
+  function calculateTotal() {
+    let sum = 0;
+    state.cart.forEach(item => {
+      sum += item.price * item.purchaseQuantity;
+    });
+    return sum.toFixed(2);
+  }
+
+  function submitCheckout() {
+    const productIds = [];
+
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+
+    getCheckout({
+      variables: { products: productIds }
+    });
   }
 
   if (!state.cartOpen) {
@@ -37,14 +71,6 @@ const Cart = () => {
     );
   }
 
-  function calculateTotal() {
-    let sum = 0;
-    state.cart.forEach(item => {
-      sum += item.price * item.purchaseQuantity;
-    });
-    return sum.toFixed(2);
-  }
-
   return (
     <div className="cart">
       <div className="close" onClick={toggleCart}>[close]</div>
@@ -54,15 +80,17 @@ const Cart = () => {
           {state.cart.map(item => (
             <CartItem key={item._id} item={item} />
           ))}
-          <div className="flex-row space-betwen">
+
+          <div className="flex-row space-between">
             <strong>Total: ${calculateTotal()}</strong>
+
             {
               Auth.loggedIn() ?
-              <button>
-                Checkout
-              </button>
-              :
-              <span>(log in to check out)</span>
+                <button onClick={submitCheckout}>
+                  Checkout
+                </button>
+                :
+                <span>(log in to check out)</span>
             }
           </div>
         </div>
@@ -71,7 +99,7 @@ const Cart = () => {
           <span role="img" aria-label="shocked">
             😱
           </span>
-          You havn't added anything to your cart yet!
+          You haven't added anything to your cart yet!
         </h3>
       )}
     </div>
